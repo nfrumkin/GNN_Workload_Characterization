76d53a63e7104fb3f13794acc5f5b602fd18a23b
Natalia Frumkin <nfrumkin@utexas.edu>
2021-04-26 15:40:51 -0400

    add pyGAT test files

---
+++ pyGAT/test.py
@@ -0,0 +1,104 @@
+from __future__ import division
+from __future__ import print_function
+
+import os
+import glob
+import time
+import random
+import argparse
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
+
+from utils import load_data, accuracy
+from models import GAT, SpGAT
+
+# Training settings
+parser = argparse.ArgumentParser()
+parser.add_argument('--no-cuda', action='store_true', default=False, help='Disables CUDA training.')
+parser.add_argument('--fastmode', action='store_true', default=False, help='Validate during training pass.')
+parser.add_argument('--sparse', action='store_true', default=False, help='GAT with sparse version or not.')
+parser.add_argument('--seed', type=int, default=72, help='Random seed.')
+parser.add_argument('--time-file', type=str, default='', help='timing output file')
+parser.add_argument('--pkl-file', type=str, default='trained-model.pkl', help='trained model input file (pkl)')
+
+args = parser.parse_args()
+args.cuda = not args.no_cuda and torch.cuda.is_available()
+
+random.seed(args.seed)
+np.random.seed(args.seed)
+torch.manual_seed(args.seed)
+if args.cuda:
+    torch.cuda.manual_seed(args.seed)
+
+# Load data
+adj, features, labels, idx_train, idx_val, idx_test = load_data()
+
+# Model and optimizer
+if args.sparse:
+    model = SpGAT(nfeat=features.shape[1], 
+                nhid=args.hidden, 
+                nclass=int(labels.max()) + 1, 
+                dropout=args.dropout, 
+                nheads=args.nb_heads, 
+                alpha=args.alpha)
+else:
+    model = GAT(nfeat=features.shape[1], 
+                nhid=args.hidden, 
+                nclass=int(labels.max()) + 1, 
+                dropout=args.dropout, 
+                nheads=args.nb_heads, 
+                alpha=args.alpha)
+optimizer = optim.Adam(model.parameters(), 
+                       lr=args.lr, 
+                       weight_decay=args.weight_decay)
+
+if args.cuda:
+    model.cuda()
+    features = features.cuda()
+    adj = adj.cuda()
+    labels = labels.cuda()
+    idx_train = idx_train.cuda()
+    idx_val = idx_val.cuda()
+    idx_test = idx_test.cuda()
+
+features, adj, labels = Variable(features), Variable(adj), Variable(labels)
+
+def compute_test():
+    model.eval()
+    output = model(features, adj)
+    loss_test = F.nll_loss(output[idx_test], labels[idx_test])
+    acc_test = accuracy(output[idx_test], labels[idx_test])
+    print("Test set results:",
+          "loss= {:.4f}".format(loss_test.data.item()),
+          "accuracy= {:.4f}".format(acc_test.data.item()))
+
+
+model.load_state_dict(torch.load(args.pkl_file))
+
+def time_model(model, file):
+    n_warmup = 50
+    n_sample = 50
+    for i in range(0,n_warmup):
+        output = model(features, adj)
+
+    tic = time.perf_counter()
+    for i in range(0,n_sample):
+        output = model(features, adj)
+    toc = time.perf_counter()
+    avg_runtime = float(toc - tic)/n_sample
+    print("average runtime = ", avg_runtime)
+
+    # write runtime to file
+    f = open(file, "w")
+    f.write(str(avg_runtime)+"\n")
+    f.close()
+
+if len(args.time_file) != 0: # time and send time to file
+    time_model(model, args.time_file)
+
+compute_test()
+
