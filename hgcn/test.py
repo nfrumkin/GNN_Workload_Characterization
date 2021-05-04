518e779a1fbddb155111116f8510ff14a77f9a2e
Natalia Frumkin <nfrumkin@utexas.edu>
2021-05-03 22:21:43 -0400

    add hgcn test

---
+++ hgcn/test.py
@@ -0,0 +1,95 @@
+from __future__ import division
+from __future__ import print_function
+
+import datetime
+import json
+import logging
+import os
+import pickle
+import time
+
+import numpy as np
+import optimizers
+import torch
+from config import parser
+from models.base_models import NCModel, LPModel
+from utils.data_utils import load_data
+from utils.train_utils import get_dir_name, format_metrics
+
+
+def test(args):
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    if int(args.double_precision):
+        torch.set_default_dtype(torch.float64)
+    if int(args.cuda) >= 0:
+        torch.cuda.manual_seed(args.seed)
+    args.device = 'cuda:' + str(args.cuda) if int(args.cuda) >= 0 else 'cpu'
+    args.patience = args.epochs if not args.patience else  int(args.patience)
+    logging.getLogger().setLevel(logging.INFO)
+    if args.save:
+        if not args.save_dir:
+            dt = datetime.datetime.now()
+            date = f"{dt.year}_{dt.month}_{dt.day}"
+            models_dir = os.path.join(os.environ['LOG_DIR'], args.task, date)
+            save_dir = get_dir_name(models_dir)
+        else:
+            save_dir = args.save_dir
+        logging.basicConfig(level=logging.INFO,
+                            handlers=[
+                                logging.FileHandler(os.path.join(save_dir, 'log.txt')),
+                                logging.StreamHandler()
+                            ])
+
+    logging.info(f'Using: {args.device}')
+    logging.info("Using seed {}.".format(args.seed))
+
+    # Load data
+    data = load_data(args, os.path.join(os.environ['DATAPATH'], args.dataset))
+    args.n_nodes, args.feat_dim = data['features'].shape
+    if args.task == 'nc':
+        Model = NCModel
+        args.n_classes = int(data['labels'].max() + 1)
+        logging.info(f'Num classes: {args.n_classes}')
+    else:
+        args.nb_false_edges = len(data['train_edges_false'])
+        args.nb_edges = len(data['train_edges'])
+        if args.task == 'lp':
+            Model = LPModel
+        else:
+            Model = RECModel
+            # No validation for reconstruction task
+            args.eval_freq = args.epochs + 1
+
+    if not args.lr_reduce_freq:
+        args.lr_reduce_freq = args.epochs
+
+    # Model and optimizer
+    model = Model(args)
+    checkpoint_path="logs/lp/2021_5_3/0/model.pth"
+    model.load_state_dict(torch.load(checkpoint_path))
+    logging.info(str(model))
+    optimizer = getattr(optimizers, args.optimizer)(params=model.parameters(), lr=args.lr,
+                                                    weight_decay=args.weight_decay)
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(
+        optimizer,
+        step_size=int(args.lr_reduce_freq),
+        gamma=float(args.gamma)
+    )
+    tot_params = sum([np.prod(p.size()) for p in model.parameters()])
+    logging.info(f"Total number of parameters: {tot_params}")
+    if args.cuda is not None and int(args.cuda) >= 0 :
+        os.environ['CUDA_VISIBLE_DEVICES'] = str(args.cuda)
+        model = model.to(args.device)
+        for x, val in data.items():
+            if torch.is_tensor(data[x]):
+                data[x] = data[x].to(args.device)
+
+
+    model.eval()  # set evaluation mode
+    embeddings = model.encode(data['features'], data['adj_train_norm'])
+    val_metrics = model.compute_metrics(embeddings, data, 'val')
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    test(args)
