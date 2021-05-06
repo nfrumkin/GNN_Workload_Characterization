98956fe23fb018d5fdac9b504c22c7f68ea8ed82
Noah Rose <45741097+noahrose18@users.noreply.github.com>
2021-05-05 19:44:24 -0500

    profiler

---
+++ pyGAT/profiler.py
@@ -0,0 +1,121 @@
+from __future__ import division
+from __future__ import print_function
+
+import os
+import glob
+import time
+import random
+import argparse
+import numpy as np
+import torch
+import torchvision.models as models
+import torch.autograd.profiler as profiler
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
+
+
+from utils import load_data, accuracy
+from models import GAT, SpGAT
+
+# Training settings
+parser = argparse.ArgumentParser()
+parser.add_argument('--no-cuda', action='store_true', default=False, help='Disables CUDA training.')
+parser.add_argument('--fastmode', action='store_true', default=False, help='Validate during training pass.')
+parser.add_argument('--sparse', action='store_true', default=False, help='GAT with sparse version or not.')
+parser.add_argument('--epochs', type=int, default=10000, help='Number of epochs to train.')
+parser.add_argument('--lr', type=float, default=0.005, help='Initial learning rate.')
+parser.add_argument('--weight_decay', type=float, default=5e-4, help='Weight decay (L2 loss on parameters).')
+parser.add_argument('--hidden', type=int, default=8, help='Number of hidden units.')
+parser.add_argument('--nb_heads', type=int, default=8, help='Number of head attentions.')
+parser.add_argument('--dropout', type=float, default=0.6, help='Dropout rate (1 - keep probability).')
+parser.add_argument('--alpha', type=float, default=0.2, help='Alpha for the leaky_relu.')
+parser.add_argument('--patience', type=int, default=100, help='Patience')
+parser.add_argument('--seed', type=int, default=72, help='Random seed.')
+parser.add_argument('--time_file', type=str, default='', help='timing output file')
+parser.add_argument('--pkl_file', type=str, default='trained-model.pkl', help='trained model input file (pkl)')
+
+args = parser.parse_args()
+args.cuda = not args.no_cuda and torch.cuda.is_available()
+random.seed(args.seed)
+np.random.seed(args.seed)
+torch.manual_seed(args.seed)
+if args.cuda:
+    torch.cuda.manual_seed(args.seed)
+
+# Load data
+adj, features, labels, idx_train, idx_val, idx_test = load_data()
+
+# Model and optimizer
+if args.sparse:
+    model = SpGAT(nfeat=features.shape[1], 
+                nhid=args.hidden, 
+                nclass=int(labels.max()) + 1, 
+                dropout=args.dropout, 
+                nheads=args.nb_heads, 
+                alpha=args.alpha)
+else:
+    model = GAT(nfeat=features.shape[1], 
+                nhid=args.hidden, 
+                nclass=int(labels.max()) + 1, 
+                dropout=args.dropout, 
+                nheads=args.nb_heads, 
+                alpha=args.alpha)
+optimizer = optim.Adam(model.parameters(), 
+                       lr=args.lr, 
+                       weight_decay=args.weight_decay)
+
+if args.cuda:
+    model.cuda()
+    features = features.cuda()
+    adj = adj.cuda()
+    labels = labels.cuda()
+    idx_train = idx_train.cuda()
+    idx_val = idx_val.cuda()
+    idx_test = idx_test.cuda()
+
+features, adj, labels = Variable(features), Variable(adj), Variable(labels)
+
+def compute_test():
+    with profiler.profile(record_shapes=True) as prof:
+        with profiler.record_function("model_infrerence"):
+            model.eval()
+            output = model(features, adj)
+            loss_test = F.nll_loss(output[idx_test], labels[idx_test])
+            acc_test = accuracy(output[idx_test], labels[idx_test])
+            print("Test set results:",
+                "loss= {:.4f}".format(loss_test.data.item()),
+                "accuracy= {:.4f}".format(acc_test.data.item()))
+    print(prof.key_averages().table(sort_by="cpu_time_total", row_limit=10))
+    print(prof.key_averages().table(sort_by="cpu_memory_usage", row_limit=10))
+
+def time_model(file):
+    model.eval()
+    n_warmup = 50
+    n_sample = 50
+    print("=== Running Warmup Passes")
+    for i in range(0,n_warmup):
+        output = model(features, adj)
+    
+    print("=== Collecting Runtime over ", str(n_sample), " Passes")
+    tic = time.perf_counter()
+    for i in range(0,n_sample):
+        output = model(features, adj)
+    toc = time.perf_counter()
+    avg_runtime = float(toc - tic)/n_sample
+    print("average runtime = ", avg_runtime)
+
+    # write runtime to file
+    f = open(file, "w")
+    f.write(str(avg_runtime)+"\n")
+    f.close()
+
+if __name__ == "__main__":
+    map_location=torch.device('cpu')
+    model.load_state_dict(torch.load(args.pkl_file))
+
+    if len(args.time_file) != 0: # time and send time to file
+        time_model(args.time_file)
+
+    compute_test()
